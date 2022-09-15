9a3be7ee2e4ed6a2b5823d62f8f90d5f7324908f
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-09-15 12:21:12 -0300

    chore: add typescript

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
