21cdbb7186436ccdb97abb58159647bc23c7146b
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-09-15 12:22:05 -0300

    feat(Header): add component Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StatusBar, Text } from "react-native";
+import { styles } from "./styles";
+
+export default function Header() {
+  return (
+    <SafeAreaView style={styles.headerContainer}>
+      <StatusBar animated={true} barStyle='light-content' />
+      <Text style={styles.title}>Super Calculadora de IMC</Text>
+    </SafeAreaView>
+  );
+}
---
+++ components/Header/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  headerContainer: {
+    height: 160,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    backgroundColor: "#2c3e50",
+  },
+  title: {
+    color: "#ecf0f1",
+    fontSize: 20,
+    fontWeight: "800",
+    marginTop: 80,
+    padding: 10,
+  }
+});
