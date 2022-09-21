71aa4c0b982874bd5ebcaa0b40579fc3320bf394
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-09-21 09:39:16 -0300

    feat(Form): add mascara para input de altura

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,6 +2,12 @@ import { View, TextInput } from "react-native";
 import { styles } from "./styles";
 
 export default function Form({height, setHeight, weight, setWeight}) {
+  const heightMask = (value: string):string => {
+    value = value.replace(/\D/g, '');
+    value = value.replace(/^(\d{1})(\d)/, '$1,$2')
+    return value;
+  }
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -10,7 +16,7 @@ export default function Form({height, setHeight, weight, setWeight}) {
         placeholderTextColor='#7f8c8d'
         style={styles.input}
         value={height}
-        onChangeText={setHeight}
+        onChangeText={(value) => setHeight(heightMask(value))}
       />
       <TextInput
         keyboardType='numeric'
