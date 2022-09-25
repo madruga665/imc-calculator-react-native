d859caf74e90db9d12fb1c8cee60d226166ec8b5
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-09-25 07:46:58 -0300

    chore(App): converte a declaração do componente pra arrow function

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import Routes from "./src/routes";
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <>
       <Routes />
       <StatusBar style='light' />
     </>
   );
-}
+};
+
+export default App;
