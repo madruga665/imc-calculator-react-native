09680f7c8f00b4c1cb5857dd54cde58b4c8f4738
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-09-25 07:31:41 -0300

    chore(Header): converte a declaração do componente pra arrow function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, Text } from "react-native";
 import { styles } from "./styles";
 
-export default function Header() {
+const Header: React.FC = () => {
   return (
     <SafeAreaView style={styles.headerContainer}>
       <Text style={styles.title}>Super Calculadora de IMC</Text>
     </SafeAreaView>
   );
-}
+};
+
+export default Header;
